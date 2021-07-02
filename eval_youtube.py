5e1d9adffb69344277eb2b3ced12a26ae12ec961
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-02 19:29:22 +0700

    Update eval_youtube.py

--- eval_youtube.py
+++ eval_youtube.py
@@ -101,7 +101,9 @@ for data in progressbar(test_loader, max_value=len(test_loader), redirect_stdout
         this_out_path = path.join(out_path, name, eid)
         os.makedirs(this_out_path, exist_ok=True)
     #     export_frames = meta_exp['videos'][name]['frames']
-        msk = msk.detach().cpu().numpy()[0].astype(np.uint8)
+        msk = msk.detach().cpu().numpy().astype(np.uint8)
+        print(msk.shape)
+        msk = msk[0]
         for f in range(msk.shape[0]):
             img_E = Image.fromarray(msk[f][0])
             img_E.save(os.path.join(this_out_path, info['frames'][f][0].replace('.jpg','.png')))
