e6a5fb6334f5acc77d9e4093630202a06ec14ed7
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-06 17:12:25 +0700

    Update select_mask.py

--- select_mask.py
+++ select_mask.py
@@ -61,7 +61,7 @@ def prepare_data(vid, eid, frame_ids):
         info['label_backward'][idx] = l
         idx += 1
     masks = torch.from_numpy(all_to_onehot(masks, labels)).float()
-
+    print(info)
     masks = mask_transform(masks)
     masks = masks.unsqueeze(2)
 
