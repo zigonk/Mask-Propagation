b15193509095bb43266afed47a6842c330c839a2
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-06 17:50:21 +0700

    Update select_mask.py

--- select_mask.py
+++ select_mask.py
@@ -112,8 +112,6 @@ def propagate(data, prop_model):
 def compare_iou(mask1, mask2):
     mask1 = np.asarray(np.asarray(mask1) > 0)
     mask2 = np.asarray(np.asarray(mask2) > 0)
-    print(mask1.shape)
-    print(mask2.shape)
     inter = np.sum(mask1 * mask2)
     union = np.sum(mask1 + mask2) - inter
     return inter / (union + 1e-6)
